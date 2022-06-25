04d7b9caf0f6ecc53869f15d25df06c7e2c9369e
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-06-24 21:44:08 -0400

    updated Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
-
+import java.util.Scanner
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-        System.out.println("hello");
+        Scanner scan = new Scanner(System.in);
+        String name;
+        System.out.println("what's your name?");
+        name = scan.nextLine();
+
+        if (name.equals("Alice" || "Bob")) {
+            System.out.println("Greetings!!");
+        } else {
+            System.out.println("Goodbye");
+        }
     }
 }
