630bc127d3b22a4351c2d2d781f3d492ced69904
Nick Choi <96934316+nickchoi10@users.noreply.github.com>
2022-07-02 23:06:01 -0400

    separate into 2 classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,41 @@
-import java.util.Scanner
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args ){
+    //    public static void main(String[] args ){
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("what's your name?");
+//        String name = scan.nextLine();
+//
+//        if (name=="Alice") {
+//            System.out.println("Greetings Alice!!!");
+//        }if (name == "Bob") {
+//            System.out.println("Greetings Bob!!!");
+//        }else {
+//            System.out.println("I will not greet you.");
+//        }
+//    }
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String name;
-        System.out.println("what's your name?");
-        name = scan.nextLine();
+        System.out.println("What is your name?");
+        String name = scan.nextLine(); //getting input from user...
+        Main names = new Main();
+        names.AliceAndBob(name);
 
-        if (name.equals("Alice" || "Bob")) {
-            System.out.println("Greetings!!");
-        } else {
-            System.out.println("Goodbye");
+    }
+
+    public void AliceAndBob(String name) {
+        if (name.equalsIgnoreCase("Alice")) {
+            System.out.println("Hello Alice");
+             }
+            else if (name.equalsIgnoreCase("Bob")){
+                System.out.println("Hello Bob");
+            }
+
         }
     }
-}
+
+
+
